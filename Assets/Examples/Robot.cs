573964874cba83c0ac918952d139f93df530fb3e
neyneyalldayday <drfrankenstein123@gmail.com>
2025-02-19 19:24:40 -0600

    playing with classes

--- Assets/Examples/Robot.cs
+++ Assets/Examples/Robot.cs
@@ -1,8 +1,28 @@
+using UnityEngine;
+// RPG is the main namespace 
+// for our game
+
 namespace RPG.Example
 {
-    class Robot
+    public class Robot : MonoBehaviour
     {
-        
+        /*
+        Variables
+        ============
+        n stuff
+        */
+
+        public int age = 5;
+        private float price = 99.99f;
+        //private string name = "McBot";
+        public bool isTurnedOn = false;
+
+        public Robot()
+        {
+            isTurnedOn = true; 
+            
+        }
+
     }
 
 }
