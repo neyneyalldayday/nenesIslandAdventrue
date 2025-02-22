5207d9b0c5a6329c46da5b939bd07a598d788ad2
neyneyalldayday <drfrankenstein123@gmail.com>
2025-02-22 10:42:23 -0600

    just messin

--- Assets/Examples/Robot.cs
+++ Assets/Examples/Robot.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 // RPG is the main namespace 
 // for our game
@@ -6,46 +7,52 @@ namespace RPG.Example
 {
     public class Robot : MonoBehaviour
     {
-        /*
-        Variables
-        ============
-        n stuff
-        */
+        // /*
+        // Variables
+        // ============
+        // n stuff
+        // */
 
-        public int age = 5;
-        public float price = 99.99f;
-        //private string name = "McBot";
-        public bool isTurnedOn = false;
+        // public int age = 5;
+        // public float price = 99.99f;
+        // //private string name = "McBot";
+        // public bool isTurnedOn = false;
 
-        public Robot()
-        {
-            //isTurnedOn = true; 
-            float newPrice = CaluclatePrice(0.7f, 1);
+        // public Robot()
+        // {
+        //     //isTurnedOn = true; 
+        //     float newPrice = CaluclatePrice(0.7f, 1);
 
-            if(newPrice > 75f)
-            {
-                price = newPrice;
-            } 
-            else 
-            {
-                // print("Price is too low foo!!!!");            
-                // Debug.LogError("you suck hard");
-                Log("you suck bitch");
-                Log<int>(age);
-                Log(isTurnedOn);
-            }
+        //     if(newPrice > 75f)
+        //     {
+        //         price = newPrice;
+        //     } 
+        //     else 
+        //     {
+        //         // print("Price is too low foo!!!!");            
+        //         // Debug.LogError("you suck hard");
+        //         Log("you suck bitch");
+        //         Log<int>(age);
+        //         Log(isTurnedOn);
+        //     }
             
-        }
+        // }
 
-        public float CaluclatePrice( float discount, int quantity )
-        {
-            return (price - ( price * discount )) * quantity; 
-        }
+        // [Obsolete("calculate price is deprecated use ApplyDiscount instead")]
+        // public float CaluclatePrice( float discount, int quantity )
+        // {
+        //     return (price - ( price * discount )) * quantity; 
+        // }
 
-        public void Log<Placeholder>(Placeholder message)
-        {
-            Debug.Log(message);
-        }
+        // public float ApplyDiscount( float discount )
+        // {
+        //     return (price - ( price * discount )); 
+        // }
+
+        // public void Log<Placeholder>(Placeholder message)
+        // {
+        //     Debug.Log(message);
+        // }
        
 
     }
