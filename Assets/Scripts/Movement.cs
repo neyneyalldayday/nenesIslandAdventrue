eae143d3a5c88a3fefb50ceefff0c982f73e32da
neyneyalldayday <drfrankenstein123@gmail.com>
2025-06-20 13:47:35 -0500

    a lil log

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+
+namespace RPG.Character
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Movement : MonoBehaviour
     {
-        
+        public void HandleMove()
+        {
+            print("WASD keys pressed");
+        }
+    // Start is called before the first frame update
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
+
