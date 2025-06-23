8074d3db11377bd63da4619e25a3c9e9028310c7
neyneyalldayday <drfrankenstein123@gmail.com>
2025-06-23 14:47:12 -0500

    set up basic movement no camera follow and speed too fast

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
 
 
 namespace RPG.Character
 {
     public class Movement : MonoBehaviour
     {
-        public void HandleMove()
+        private NavMeshAgent agent;
+        private Vector3 movementVector;
+
+        private void Awake()
         {
-            print("WASD keys pressed");
+            agent = GetComponent<NavMeshAgent>();
+        } 
+
+       private void Update()
+        {
+            MovePlayer();   
+        }
+
+        private void MovePlayer()
+        {
+            agent.Move(movementVector);
+        }
+
+        public void HandleMove(InputAction.CallbackContext context)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
+            movementVector = new Vector3(input.x, 0, input.y);
+
+            print(movementVector);
         }
     // Start is called before the first frame update
 
