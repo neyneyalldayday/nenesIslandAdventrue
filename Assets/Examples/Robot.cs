71b794f708e4481e87f1536532dab91c32c86d7b
neyneyalldayday <drfrankenstein123@gmail.com>
2025-07-24 14:56:02 -0500

    classes and static variables

--- Assets/Examples/Robot.cs
+++ Assets/Examples/Robot.cs
@@ -13,6 +13,10 @@ namespace RPG.Example
         {
             includedBattery = new Battery(80f);
             print(includedBattery.health);
+            Charger.ChargeBattery(includedBattery);
+            print(includedBattery.health);
+            print(Charger.chargerInUse);
+            
         }
     }
 
@@ -24,6 +28,17 @@ namespace RPG.Example
         {
             health = newHealth;
             Debug.Log("New battery created");
+        }
+    }
+
+    static class Charger
+    {
+        public static bool chargerInUse = false;
+
+        public static void ChargeBattery(Battery batterToCharge)
+        {
+            chargerInUse = true;
+            batterToCharge.health = 100f;
         }
     }
 
